3b1e07bde3dc7df8ee230a2a577a8b2581ad4db4
fufuu <598409@egat.co.th>
2023-09-28 13:32:52 +0700

    add::marker on google map

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
@@ -8,9 +12,28 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  final _controller = Completer<GoogleMapController>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text('map')), body: const Text('test'));
+        appBar: AppBar(title: const Text('map')),
+        body: GoogleMap(
+          markers: <Marker>{
+            Marker(markerId: const MarkerId('id01'),
+            position: const LatLng(13.9166382, 100.5475167),
+            infoWindow: InfoWindow(
+              title: 'IMM',
+              snippet: 'report',
+              onTap: () {
+                Get.snackbar('Welcome', 'IMM');
+              }
+            )),
+          },
+          mapType: MapType.normal,
+          initialCameraPosition: const CameraPosition(
+              target: LatLng(13.9166382, 100.5475167), zoom: 16),
+          onMapCreated: (controller) => _controller.complete(controller),
+        ));
   }
 }
